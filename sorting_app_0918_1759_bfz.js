04d379931b502332ac872cbcbc8942eb1d763ba3
ViNcentLUIS765 <spoof-05.folds@icloud.com>
2025-09-18 11:59:15 +0200

    Enhance 排序算法实现 using ELECTRON

---
+++ sorting_app_0918_1759_bfz.js
@@ -0,0 +1,89 @@
+// 代码生成时间: 2025-09-18 17:59:14
+const { app, BrowserWindow } = require('electron');
+
+// 定义排序算法
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+function selectionSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
+
+function insertionSort(arr) {
+  let len = arr.length;
+  for (let i = 1; i < len; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+// 创建主窗口函数
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile('index.html');
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+// 当ELECTRON完成初始启动并准备好创建浏览器窗口时，调用此函数
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// 导出排序算法函数，以便在主窗口中使用
+module.exports = {
+  bubbleSort,
+  selectionSort,
+  insertionSort,
+};
